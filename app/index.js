9f7918a6437af904de2512ef0cf56ba5920d8aff
Amir Rahi <amirrahi29@gmail.com>
2026-04-11 16:32:43 +0530

    some changes

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.type("html").send(`<!DOCTYPE html>
 <html lang="en">
-<head><meta charset="utf-8"><title>Welcome</title></head>
+<head><meta charset="utf-8"><title>Welcome main branch</title></head>
 <body>
   <h1>Welcome! main branch </h1>
   <p><a href="/data">GET /data</a> — JSON response</p>
